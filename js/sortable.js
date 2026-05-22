b009da6957fc1e4acbfe346cb281657faad29cac
NathanRusso <nathandanielrusso@gmail.com>
2026-05-22 12:23:19 -0400

    I reworked the website using SortableJS.

--- js/sortable.js
+++ js/sortable.js
@@ -1,15 +1,45 @@
 const body = document.body;
 let currentAcademicYear = 1; // The numbers of years of school currently being listed.
 
+const fall_1 = document.getElementById(`fall-${currentAcademicYear}`);
+const spring_1 = document.getElementById(`spring-${currentAcademicYear}`);
+const summer_1 = document.getElementById(`summer-${currentAcademicYear}`);
+makeSortable(fall_1);
+makeSortable(spring_1);
+makeSortable(summer_1);
+
+/**
+ * This makes the given div element sortable.
+ * 
+ * @param {*} element - the given semester div
+ */
+function makeSortable(element) {
+    Sortable.create(element, {
+        group: "semester",
+        animation: 200,
+        onMove: function (event, originalEvent) {
+            const movingElement = event.dragged;
+            const targetElement = event.to;
+            const targetChildren = targetElement.children;
+            if (targetChildren.length > 0) {
+                // Note: Works because both "class" and "co-op" are length 5
+                let movingClass = movingElement.className.slice(0, 5);
+                let targetClass = targetChildren[0].className.slice(0, 5);
+                return movingClass == targetClass;
+            }
+        }
+    });
+}
+
 /**
  * This appends a new year divider and year element to the body.
  * The html being created is as follows:
  * 
  * <div id="year-divider-#" class="year-divider"></div>
  * <div id="year-#">
- *     <div id="fall-# class="semester" ondrop="dropHandler(event)" ondragover="dragoverHandler(event)"></div>
- *     <div id="spring-#" class="semester" ondrop="dropHandler(event)" ondragover="dragoverHandler(event)"></div>
- *     <div id="summer-#" class="semester" ondrop="dropHandler(event)" ondragover="dragoverHandler(event)"></div>
+ *     <div id="fall-# class="semester"></div>
+ *     <div id="spring-#" class="semester"></div>
+ *     <div id="summer-#" class="semester"></div>
  * </div>
  */
 function addYear() {
@@ -28,8 +58,7 @@ function addYear() {
         const semesterDiv = document.createElement("div");
         semesterDiv.id = `${term}-${currentAcademicYear}`;
         semesterDiv.className = "semester";
-        semesterDiv.ondrop = dropHandler;
-        semesterDiv.ondragover = dragoverHandler;
+        makeSortable(semesterDiv);
         yearDiv.appendChild(semesterDiv);
     });
     body.appendChild(yearDiv);
@@ -39,6 +68,7 @@ function addYear() {
  * This removes a year divider and year element from the body.
  */
 function removeYear() {
+    if (currentAcademicYear == 1) return;
     console.log(`Remove Year ${currentAcademicYear}`);
     document.getElementById(`year-divider-${currentAcademicYear}`).remove();
     document.getElementById(`year-${currentAcademicYear}`).remove();
