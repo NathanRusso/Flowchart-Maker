52cef54471fea478d391b43d61f638e008af26ed
NathanRusso <nathandanielrusso@gmail.com>
2026-07-04 16:10:09 -0400

    I updated flowchart notes.

--- js/main.js
+++ js/main.js
@@ -976,13 +976,13 @@ function getAttributeColor(attribute) {
             || attribute.includes("NMID") || attribute.includes("NSSA") || attribute.includes("SE") 
             || attribute.includes("SWEN")) {
         return "DarkOrange";
-    } else if (attribute.includes("Gen Ed") || attribute.includes("Writing Intensive") || attribute.includes("Professional Elective")) {
+    } else if (attribute.includes("Gen Ed") || attribute.includes("Writing Intensive") || attribute == "Professional Elective") {
         return "Green";
     } else if (attribute.includes("Lab Science")) {
         return "Crimson";
     } else if (attribute.includes("Math/Science")) {
         return "DarkViolet"
-    } else if ( attribute.includes("DHSS") ) {
+    } else if (attribute.includes("DHSS")) {
         return "HotPink";
     } else {
         return "var(--text-color)"
